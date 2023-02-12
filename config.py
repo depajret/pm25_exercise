dec57e5baf74fe0a3007ac4ff500ff8fee68942b
Bartek <b.wieczorek@student.uw.edu.pl>
2023-02-12 21:52:10 +0100

    Add config

---
+++ config.py
@@ -0,0 +1,5 @@
+TEST_CITY = "Shanghai"
+VALIDATION_DATE = "2015-01-01"
+PATTERN_FOR_TARGET = "PM"
+TARGET_VARIABLE = "PM"
+VARIABLES_CONSIDERED = ["date", "city", "PM", "HUMI", "PRES", "TEMP", "Iws", "precipitation", "Iprec"]
