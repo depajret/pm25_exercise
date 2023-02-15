f2fe65cbeb0ab3a09c9fbb15f7544dcc1c763dde
Bartek <b.wieczorek@student.uw.edu.pl>
2023-02-15 01:06:41 +0100

    Add config variables

--- config.py
+++ config.py
@@ -5,3 +5,12 @@ TARGET_VARIABLE = "PM"
 VARIABLES_CONSIDERED = ["date", "city", "PM", "HUMI", "PRES", "TEMP", "Iws", "precipitation", "Iprec"]
 NUMERICAL_VARIABLES = ["HUMI", "PRES", "TEMP", "Iws", "precipitation", "Iprec"]
 CITIES = ["Beijing", "Chengdu", "Guangzhou", "Shanghai", "Shenyang"]
+
+
+EPOCHS = 20
+BATCH_SIZE = 12
+LEARNING_RATE = 0.01
+NUM_HIDDEN_UNITS = 16
+NUM_LAYERS = 2
+DROPOUT = 0.1
+SEQUENCE_LENGTH = 6
